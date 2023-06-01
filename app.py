0ef9cb3be26d9fad9a0f6f7fa36b060c1b540804
khadija3818 <126562801+khadija3818@users.noreply.github.com>
2023-06-01 12:19:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-!pip install streamlit
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-# File uploader
-uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Button to process data
-if st.button("Process"):
-    if uploaded_file is not None:
-        data = pd.read_csv(uploaded_file)
-        st.success("File uploaded and processed successfully!")
-    else:
-        st.warning("Please upload a CSV file.")
-# Display uploaded data
-if uploaded_file is not None:
-    st.subheader("Uploaded Data")
-    st.write(data)
+# Load the movie ratings data
+ratings_data = pd.read_csv('ratings.csv')
 
-    # Generate a bar plot
-    st.subheader("Bar Plot")
-    plt.bar(data['x'], data['y'])
-    st.pyplot()
+# Load the movie metadata
+movies_metadata = pd.read_csv('movies.csv')
 
+# Create a pivot table of user ratings
+user_ratings = ratings_data.pivot_table(index='userId', columns='movieId', values='rating')
+
+# Calculate item-item similarity using cosine similarity
+item_similarity = cosine_similarity(user_ratings.fillna(0))
+
+# Streamlit app
+st.title('Movie Recommendation System')
+
+# User input for movie selection
+selected_movie = st.selectbox('Select a movie:', movies_metadata['title'].values)
+
+# Get the index of the selected movie
+selected_movie_index = movies_metadata[movies_metadata['title'] == selected_movie].index[0]
+
+# Get similar movies based on item-item similarity
+similar_movies = list(enumerate(item_similarity[selected_movie_index]))
+
+# Sort similar movies by similarity score
+sorted_similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)
+
+# Display recommended movies
+st.subheader('Recommended Movies:')
+
+for i in range(5):
+    movie_index = sorted_similar_movies[i][0]
+    recommended_movie = movies_metadata.iloc[movie_index]['title']
+    st.write(f'{i+1}. {recommended_movie}')
