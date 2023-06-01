e37e7bdd879f906eefb19243af48cea838bd3687
khadija3818 <126562801+khadija3818@users.noreply.github.com>
2023-06-01 12:23:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,16 @@
 import streamlit as st
-import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
 
-# Load the movie ratings data
-ratings_data = pd.read_csv('ratings.csv')
-
-# Load the movie metadata
-movies_metadata = pd.read_csv('movies.csv')
-
-# Create a pivot table of user ratings
-user_ratings = ratings_data.pivot_table(index='userId', columns='movieId', values='rating')
-
-# Calculate item-item similarity using cosine similarity
-item_similarity = cosine_similarity(user_ratings.fillna(0))
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 # Streamlit app
-st.title('Movie Recommendation System')
+st.title("Word Count Tool")
 
-# User input for movie selection
-selected_movie = st.selectbox('Select a movie:', movies_metadata['title'].values)
+# User input for text
+text = st.text_area("Enter your text here")
 
-# Get the index of the selected movie
-selected_movie_index = movies_metadata[movies_metadata['title'] == selected_movie].index[0]
-
-# Get similar movies based on item-item similarity
-similar_movies = list(enumerate(item_similarity[selected_movie_index]))
-
-# Sort similar movies by similarity score
-sorted_similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)
-
-# Display recommended movies
-st.subheader('Recommended Movies:')
-
-for i in range(5):
-    movie_index = sorted_similar_movies[i][0]
-    recommended_movie = movies_metadata.iloc[movie_index]['title']
-    st.write(f'{i+1}. {recommended_movie}')
+# Count the number of words when the user clicks the "Count" button
+if st.button("Count"):
+    num_words = count_words(text)
+    st.success(f"Number of words: {num_words}")
