0ddb6a65cdf631c19dcbda1ec38c2e43d035122d
khadija3818 <126562801+khadija3818@users.noreply.github.com>
2023-06-01 12:16:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,23 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-def main():
-    st.title("Data Visualization Web App")
+# File uploader
+uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
 
-    # File uploader
-    uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
-
+# Button to process data
+if st.button("Process"):
     if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        columns = df.columns.tolist()
+        data = pd.read_csv(uploaded_file)
+        st.success("File uploaded and processed successfully!")
+    else:
+        st.warning("Please upload a CSV file.")
+# Display uploaded data
+if uploaded_file is not None:
+    st.subheader("Uploaded Data")
+    st.write(data)
 
-        # Select box for choosing the column
-        selected_column = st.selectbox("Select a column", columns)
+    # Generate a bar plot
+    st.subheader("Bar Plot")
+    plt.bar(data['x'], data['y'])
+    st.pyplot()
 
-        if st.button("Plot"):
-            # Plotting the selected column as a line chart
-            plt.plot(df[selected_column])
-            plt.xlabel("Index")
-            plt.ylabel(selected_column)
-            st.pyplot(plt)
-
-if __name__ == "__main__":
-    main()
